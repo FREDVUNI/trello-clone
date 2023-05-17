88a225ae9d743de640c018c3287ce670962ef4ac
FREDVUNI <fredvuni809@gmail.com>
2023-05-17 18:43:25 +0300

    change styles and make it responsive

--- src/styles.ts
+++ src/styles.ts
@@ -2,32 +2,53 @@ import styled from "styled-components";
 
 export const AppContainer = styled.div`
   align-items: flex-start;
-  background-color: #3179ba;
+  background-color: #212121;
+  color: #fff;
   display: flex;
   flex-direction: row;
-  height: 100%;
+  min-height: 100vh;
   padding: 20px;
   width: 100%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
+
 export const ColumnContainer = styled.div`
-  background-color: #ebecf0;
+  background-color: #2b2b2b;
+  color: #fff;
   width: 300px;
   min-height: 40px;
   margin-right: 20px;
-  border-radius: 0px;
-  padding: 8px 8px;
+  border-radius: 3px;
+  padding: 8px;
   flex-grow: 0;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-right: 0;
+    margin-bottom: 20px;
+  }
 `;
+
 export const ColumnTitle = styled.div`
   padding: 6px 16px 12px;
   font-weight: bold;
 `;
+
 export const CardContainer = styled.div`
-  background-color: #fff;
+  background-color: #363636;
+  color: #fff;
   cursor: pointer;
   margin-bottom: 0.5rem;
   padding: 0.5rem 1rem;
   max-width: 300px;
-  border-radius: 0px;
+  border-radius: 3px;
   box-shadow: #091e4240 0px 1px 0px 0px;
+
+  @media (max-width: 768px) {
+    max-width: 100%;
+  }
 `;
