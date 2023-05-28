30be60eca87991216bda61ff2dd280abe2097f02
FREDVUNI <fredvuni809@gmail.com>
2023-05-28 14:03:23 +0300

    add a keypress event to add text

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { NewItemButton, NewItemContainer, NewItemInput } from "./styles";
 import { ThemeContext } from "./context/ThemeToggle";
+import { UseFocus } from "./useFocus";
 
 type Props = {
   onAdd: (text: string) => void;
@@ -9,13 +10,23 @@ type Props = {
 export const NewItemForm = ({ onAdd }: Props) => {
   const [input, setInput] = useState("");
   const { theme } = useContext(ThemeContext);
+  const inputRef = UseFocus();
+
+  const handleAddText = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onAdd(text);
+    }
+  };
+
   return (
     <NewItemContainer>
       <NewItemInput
+        ref={inputRef}
         theme={theme}
         placeholder="Enter Item here ..."
         value={input}
         onChange={(e: any) => setInput(e.target.value)}
+        onKeyPress={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(input)}>create</NewItemButton>
     </NewItemContainer>
