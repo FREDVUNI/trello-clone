6aaa7447c04bddb8321730ff13a27b4ece68c8bf
FREDVUNI <fredvuni809@gmail.com>
2023-06-03 14:40:27 +0300

    Fix: integrate the provider on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeToggle.tsx";
+import { AppProvider } from "./context/AppState.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <AppProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AppProvider>
   </React.StrictMode>
 );
