a4a40f841860b9045340c034184deeb07208a320
FREDVUNI <fredvuni809@gmail.com>
2023-06-04 11:58:46 +0300

    chore: add types for lists and tasks

--- src/context/AppState.tsx
+++ src/context/AppState.tsx
@@ -1,17 +1,24 @@
 import React, { createContext } from "react";
 
 type Node = {
-    children: React.ReactNode
+  children: React.ReactNode;
+};
+
+type Tasks = {
+  id: string;
+  text: string;
+};
+
+type Lists = {
+    id:string;
+    text:string;
+    tasks:Tasks[]
 }
 
-const initialState:any = [];
+const initialState: any = [];
 
 export const AppContext = createContext(initialState);
 
-export const AppProvider = ({children}:Node) =>{
-    return(
-        <AppContext.Provider value={{}}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+export const AppProvider = ({ children }: Node) => {
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+};
